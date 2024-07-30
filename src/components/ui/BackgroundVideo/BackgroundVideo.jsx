0a7002e3763f0147cb5be7750c555f8f2923403d
and-yevchenko <and.yecvchenko@gmail.com>
2024-07-30 22:05:35 +0300

    Fix Routing

--- src/components/ui/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/ui/BackgroundVideo/BackgroundVideo.jsx
@@ -3,7 +3,7 @@ import './BackgroundVideo.scss';
 export const BackgroundVideo = () => {
   return (
     <video autoPlay muted loop playsInline className="background-video">
-      <source src="/BackgroundVideo.mp4" type="video/mp4" />
+      <source src="./BackgroundVideo.mp4" type="video/mp4" />
     </video>
   );
 };
