a9f2ffa84a2d9c942bf65b37084c55bcd43a6626
and-yevchenko <and.yecvchenko@gmail.com>
2024-07-30 22:15:43 +0300

    Fix Routing

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -4,7 +4,7 @@ export const Logo = ({ link }) => {
   return (
     <div className="logo-aero">
       <a href={link}>
-        <img src="./Aero-create-test/Aero.svg" alt="Logo Aero" />
+        <img src="/Aero-create-test/Aero.svg" alt="Logo Aero" />
       </a>
     </div>
   );
