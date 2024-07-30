665d40723ade1183eb75f65b6c49512c9d0bb042
and-yevchenko <and.yecvchenko@gmail.com>
2024-07-30 11:10:10 +0300

    Fix RouterProvider

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -5,7 +5,7 @@ import { router } from '../Router/Router';
 function App() {
   return (
     <>
-      <RouterProvider router={router} basename="/Aero-create-test/" />
+      <RouterProvider router={router} basename="/Aero-create-test/"/>
     </>
   );
 }
